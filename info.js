8ae07c6943ddda74dffe3e9d22340d32be6b39fa
levure-s <levure.sauvage@gmail.com>
2024-02-29 19:42:43 +0900

    first_commit

---
+++ info.js
@@ -0,0 +1,90 @@
+const tweets = {
+  ayuka1: {
+    img: "ayuka1.jpg",
+    url: "https://twitter.com/Ayukababys1/status/1761731922664075752",
+  },
+  ayuka2: {
+    img: "ayuka2.jpg",
+    url: "https://twitter.com/Ayukababys1/status/1761736404634349747",
+  },
+  ageha: {
+    img: "ageha.jpg",
+    url: "https://twitter.com/ageha_Babys/status/1761733490893287698",
+  },
+  aiiro: {
+    img: "aiiro.jpg",
+    url: "https://twitter.com/aiiro_babys/status/1761735428036817203",
+  },
+  aiiro2: {
+    img: "aiiro2.jpg",
+    url: "https://twitter.com/aiiro_babys/status/1761740588121251863",
+  },
+  tan_tan_men: {
+    img: "tan_tan_men.jpg",
+    url: "https://twitter.com/tan_tan_men1024/status/1761735106941886538",
+  },
+  tan_tan_men2: {
+    img: "tan_tan_men2.jpg",
+    url: "https://twitter.com/tan_tan_men1024/status/1761737798049227033",
+  },
+  koto1: {
+    img: "koto1.jpg",
+    url: "https://twitter.com/Babys_08/status/1761737205989646695",
+  },
+  koto2: {
+    img: "koto2.jpg",
+    url: "https://twitter.com/Babys_08/status/1761737408754905362",
+  },
+  haru: {
+    img: "haru.jpg",
+    url: "https://twitter.com/haru4869_Babys/status/1761740796297122157",
+  },
+  haru2: {
+    img: "haru2.jpg",
+    url: "https://twitter.com/haru4869_Babys/status/1761740798234857502",
+  },
+  pangori: {
+    img: "pangori.jpg",
+    url: "https://twitter.com/Babys_1209/status/1761847679213273311",
+  },
+  touno: {
+    img: "touno.jpg",
+    url: "https://twitter.com/touno_karin/status/1761749033964740966",
+  },
+  touno2: {
+    img: "touno2.jpg",
+    url: "https://twitter.com/touno_karin/status/1761749335426162900",
+  },
+  yuna: {
+    img: "yuna.jpg",
+    url: "https://twitter.com/KnwRwi/status/1761743883577524716",
+  },
+  yuna2: {
+    img: "yuna2.jpg",
+    url: "https://twitter.com/KnwRwi/status/1761747378368115013",
+  },
+  nanami: {
+    img: "nanami.jpg",
+    url: "https://twitter.com/nanami_Babys/status/1761752791310274981",
+  },
+  nanami2: {
+    img: "nanami2.jpg",
+    url: "https://twitter.com/nanami_Babys/status/1761753509479977358",
+  },
+  nanami3: {
+    img: "nanami3.jpg",
+    url: "https://twitter.com/nanami_Babys/status/1761754242879287446",
+  },
+  nanami4: {
+    img: "nanami4.jpg",
+    url: "https://twitter.com/nanami_Babys/status/1761755731941114061",
+  },
+  erika: {
+    img: "erika.jpg",
+    url: "https://twitter.com/C45iv1nRVAH9Ox4/status/1761923430184824836",
+  },
+  levu: {
+    img: "levu.jpg",
+    url: "https://twitter.com/8J6pxh2o0XB5lN9/status/1761737997287059863",
+  },
+};
